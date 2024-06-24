54fe675c4be87b10cc75d7865d005f92ab392116
haidari <esmailhaidari24@gmail..com>
2024-06-24 23:47:21 +0200

    modification

--- algo age/sex.py
+++ algo age/sex.py
@@ -2,8 +2,7 @@ sex= input("veuillez saisir le sex de l'habitant h/f ")
 age= int(input("veuillez saisir l'age de l'habitant "))
 
 if (sex== 'h' and  age >=20) or (sex=='f' and age >=18 and age<= 35):
-    print('habitant est imposable')
+    print('habitant est imposable ')
      
 else:
-    
     print("l'habitant nest pasimposable")
